239950b9e012f7822b606d29a0d0039b1fc360c9
Sebastian Pettersson <sebastian.pettersson@live.com>
2021-03-16 00:58:02 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  resolve: {
+    extensions: ['.ts', '.js', 'json'],
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)$/,
+        loader: 'ts-loader',
+        exclude: [
+          [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '.serverless'),
+            path.resolve(__dirname, '.webpack'),
+          ],
+        ],
+      },
+    ],
+  },
+};
