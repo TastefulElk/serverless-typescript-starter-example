a0491c465394bf322b4308f4e261789798cfb3c8
Sebastian Pettersson <sebastian.pettersson@live.com>
2021-03-16 13:49:32 +0100

    chore: cleanup webpack resolve extensions

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
   resolve: {
-    extensions: ['.ts', '.js', 'json'],
+    extensions: ['.ts', '.tsx'],
   },
   target: 'node',
   module: {
